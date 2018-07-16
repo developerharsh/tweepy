39265f76783496379e12fb3c4ae18d26490a38ec
developerharsh <harshchauhan880@gmail.com>
2018-07-16 22:22:38 +0530

    files added

---
+++ conf.py
@@ -0,0 +1,6 @@
+consumer_key = "Consumer Key (API Key) for Twitter App"
+consumer_secret = "Consumer Secret (API Secret) for Twitter App"
+access_token = "Access Token for Twitter App"
+access_token_secret = "Access Token Secret for Twitter App"
+bolt_cloud_api_key = "API key of your Bolt Cloud"
+device_id = "ID of your Bolt Device"
